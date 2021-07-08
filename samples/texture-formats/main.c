9e5d67f33777fb3037c056dfd89c821eb1a12d7f
Mike Davis <mdavis.virtual@gmail.com>
2021-07-07 22:25:15 -0500

    Fix loading both swizzled and linear textures

--- samples/texture-formats/main.c
+++ samples/texture-formats/main.c
@@ -37,7 +37,8 @@ typedef struct Vertex {
 
 #pragma pack()
 
-static Vertex *alloc_vertices;
+static Vertex *alloc_vertices;  // texcoords 0 to width/height
+static Vertex *alloc_vertices_swizzled; // texcoords normalized 0 to 1
 static uint32_t  num_vertices;
 
 MATRIX m_model, m_view, m_proj;
@@ -53,11 +54,12 @@ VECTOR v_light_dir   = {  0,   0,   1,  1 };
 
 #define MASK(mask, val) (((val) << (ffs(mask)-1)) & (mask))
 
-struct {
+static struct {
     uint16_t width;
     uint16_t height;
     uint16_t pitch;
     void     *addr;
+    int      error;
 } texture;
 
 #define MAXRAM 0x03FFAFFF
@@ -70,7 +72,8 @@ struct {
 #define NV097_SET_TEXTURE_FORMAT_COLOR_SZ_B8G8R8A8              0x3B
 #define NV097_SET_TEXTURE_FORMAT_COLOR_LC_IMAGE_CR8YB8CB8YA8    0x24
 #define NV097_SET_TEXTURE_FORMAT_COLOR_LC_IMAGE_YB8CR8YA8CB8    0x25
-
+#define NV097_SET_TEXTURE_FORMAT_COLOR_D16                      0x2C    // TODO: proper nvidia name
+#define NV097_SET_TEXTURE_FORMAT_COLOR_LIN_F16                  0x31    // TODO: proper nvidia name
 
 static void matrix_viewport(float out[4][4], float x, float y, float width, float height, float z_min, float z_max);
 static void init_shader(void);
@@ -80,22 +83,22 @@ static void draw_arrays(unsigned int mode, int start, int count);
 
 static int format_map_index = 0;
 static const FormatInfo format_map[] = {
-    
+
     // swizzled
-    { SDL_PIXELFORMAT_ABGR8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A8B8G8R8, true, false, "SZ_A8B8G8R8" },
-    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R8G8B8A8, true, false, "SZ_R8G8B8A8" },
-    { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A8R8G8B8, true, false, "SZ_A8R8G8B8" },
-    { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_X8R8G8B8, true, false, "SZ_X8R8G8B8" },
-    { SDL_PIXELFORMAT_BGRA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_B8G8R8A8, true, false, "SZ_B8G8R8A8" },
-    { SDL_PIXELFORMAT_RGB565, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R5G6B5, true, false, "SZ_R5G6B5" },
-    { SDL_PIXELFORMAT_ARGB1555, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A1R5G5B5, true, false, "SZ_A1R5G5B5" },
-    { SDL_PIXELFORMAT_ARGB1555, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_X1R5G5B5, true, false, "SZ_X1R5G5B5" },
-    { SDL_PIXELFORMAT_ARGB4444, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A4R4G4B4, true, false, "SZ_A4R4G4B4" }, 
+    { SDL_PIXELFORMAT_ABGR8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A8B8G8R8, true, false, "A8B8G8R8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R8G8B8A8, true, false, "R8G8B8A8" },
+    { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A8R8G8B8, true, false, "A8R8G8B8" },
+    { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_X8R8G8B8, true, false, "X8R8G8B8" },
+    { SDL_PIXELFORMAT_BGRA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_B8G8R8A8, true, false, "B8G8R8A8" },
+    { SDL_PIXELFORMAT_RGB565, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R5G6B5, true, false, "R5G6B5" },
+    { SDL_PIXELFORMAT_ARGB1555, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A1R5G5B5, true, false, "A1R5G5B5" },
+    { SDL_PIXELFORMAT_ARGB1555, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_X1R5G5B5, true, false, "X1R5G5B5" },
+    { SDL_PIXELFORMAT_ARGB4444, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A4R4G4B4, true, false, "A4R4G4B4" }, 
 
     // linear unsigned
     { SDL_PIXELFORMAT_ABGR8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_A8B8G8R8, false, false, "A8B8G8R8" },
     { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_R8G8B8A8, false, false, "R8G8B8A8" },
-    { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_A8R8G8B8, false, false, "A8R8G8B8" },
+    { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_A8R8G8B8, false, false, "A8R8G8B8" },   
     { SDL_PIXELFORMAT_ARGB8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_X8R8G8B8, false, false, "X8R8G8B8" },
     { SDL_PIXELFORMAT_BGRA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_B8G8R8A8, false, false, "B8G8R8A8" },
     { SDL_PIXELFORMAT_RGB565, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_R5G6B5, false, false, "R5G6B5" },
@@ -105,12 +108,27 @@ static const FormatInfo format_map[] = {
 
     // TODO: for some that need conversion, can probably use rgba and just see if both xemu and hardware display the same garbage
 
-    // linear yuv color space
+    // yuv color space
     { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LC_IMAGE_CR8YB8CB8YA8, false, true, "YUY2" },
-    //{ SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LC_IMAGE_YB8CR8YA8CB8, false, true, "UYVY" },
-
+    //{ SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LC_IMAGE_YB8CR8YA8CB8, false, true, "UYVY" },  // TODO: implement in xemu
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_Y16, false, true, "Y16" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_Y8, true, true, "SZ_Y8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_Y8, false, true, "Y8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_AY8, true, true, "SZ_AY8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_AY8, false, true, "AY8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A8, true, true, "SZ_A8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_A8Y8, true, true, "SZ_A8Y8" },
+    
     // misc formats
-    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_Y8, true, true, "SZ_Y8" }
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_L_DXT1_A1R5G5B5, false, true, "DXT1" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_L_DXT23_A8R8G8B8, false, true, "DXT3" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_L_DXT45_A8R8G8B8, false, true, "DXT5" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_G8B8, true, true, "SZ_G8B8" },
+    //{ SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_G8B8, false, true, "G8B8" },    // TODO: implement in xemu
+    //{ SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_D16, false, true, "D16" },    // TODO: implement in xemu
+    //{ SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_LIN_F16, false, true, "LIN_F16" },    // TODO: implement in xemu
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R8B8, true, true, "SZ_R8B8" },
+    { SDL_PIXELFORMAT_RGBA8888, NV097_SET_TEXTURE_FORMAT_COLOR_SZ_R6G5B5, true, true, "R6G5B5" }
     // TODO: define others here
 };
 
@@ -119,15 +137,15 @@ int bsf(int val) {
     __asm bsf eax, val
 }
 
-bool update_texture_memory(SDL_PixelFormatEnum format, int width, int height, bool swizzled)
+int update_texture_memory(SDL_PixelFormatEnum format, int width, int height, bool swizzled)
 {
     // create source surface
     SDL_Surface *gradient_surf = SDL_CreateRGBSurfaceWithFormat(0, width, height, 32, SDL_PIXELFORMAT_RGBA8888);
-    if (!gradient_surf)
-        return false;
+    if (gradient_surf == NULL)
+        return 1;
 
-    //if (SDL_LockSurface(gradient_surf))
-    //    return false;
+    if (SDL_LockSurface(gradient_surf))
+        return 2;
 
     // generate basic gradient pattern
     uint32_t *pixels = (uint32_t*)gradient_surf->pixels;
@@ -135,17 +153,16 @@ bool update_texture_memory(SDL_PixelFormatEnum format, int width, int height, bo
         for (int x = 0; x < width; x++) {
             int xScale = x * 255.0f / width;
             int yScale = y * 255.0f / height;
-            pixels[y * width + x] = SDL_MapRGB(gradient_surf->format, xScale, yScale, xScale);
+            pixels[y * width + x] = SDL_MapRGB(gradient_surf->format, yScale, xScale, yScale);
         }
     }
 
-    //if (SDL_UnlockSurface(gradient_surf))
-    //    return false;
+    SDL_UnlockSurface(gradient_surf);
 
     // convert to desired destination format
     SDL_Surface *new_surf = SDL_ConvertSurfaceFormat(gradient_surf, format, 0);
     if (!new_surf)
-        return false;
+        return 3;
 
     SDL_FreeSurface(gradient_surf);
     if (texture.addr)
@@ -157,7 +174,7 @@ bool update_texture_memory(SDL_PixelFormatEnum format, int width, int height, bo
     uint8_t *dst_tex_buf = MmAllocateContiguousMemoryEx(height * new_surf->pitch, 0, MAXRAM, 0, PAGE_WRITECOMBINE | PAGE_READWRITE);
     if (!dst_tex_buf) {
         SDL_FreeSurface(new_surf);
-        return false;
+        return 4;
     }
 
     // copy pixels over to texture memory, swizzling if desired
@@ -167,23 +184,15 @@ bool update_texture_memory(SDL_PixelFormatEnum format, int width, int height, bo
         memcpy(dst_tex_buf, new_surf->pixels, new_surf->pitch * new_surf->h);
     }
 
-    SDL_FreeSurface(new_surf);
-
     // HACK: update global texture info
     texture.width = new_surf->w;
     texture.height = new_surf->h;
     texture.pitch = new_surf->pitch;
     texture.addr = dst_tex_buf;
 
-    // HACK: normalize texture coords based on swizzle status, not sure why this is required yet...
-    // TODO: actually width/height that need to be broken out better for non-square textures
-    for (int i = 0; i < num_vertices; i++) {
-        float val = swizzled ? 1.0f : 256.0f;
-        if (alloc_vertices[i].texcoord[0]) alloc_vertices[i].texcoord[0] = val;
-        if (alloc_vertices[i].texcoord[1]) alloc_vertices[i].texcoord[1] = val;
-    }
+    SDL_FreeSurface(new_surf);
 
-    return true;
+    return 0;
 }
 
 SDL_GameController *gameController;
@@ -223,11 +232,18 @@ int main(void)
 
     /* Load constant rendering things (shaders, geometry) */
     init_shader();
-    update_texture_memory(format_map[format_map_index].SdlFormat, 256, 256, format_map[format_map_index].XboxSwizzled);
 
+    // real nv2a hardware seems to cache this and not honor updates? have separate vertex buffers for swizzled and linear for now...
     alloc_vertices = MmAllocateContiguousMemoryEx(sizeof(vertices), 0, MAXRAM, 0, PAGE_WRITECOMBINE | PAGE_READWRITE);
+    alloc_vertices_swizzled = MmAllocateContiguousMemoryEx(sizeof(vertices), 0, MAXRAM, 0, PAGE_WRITECOMBINE | PAGE_READWRITE);
     memcpy(alloc_vertices, vertices, sizeof(vertices));
+    memcpy(alloc_vertices_swizzled, vertices, sizeof(vertices));
     num_vertices = sizeof(vertices)/sizeof(vertices[0]);
+    for (int i = 0; i < num_vertices; i++) {
+        // TODO: programmatic generation of buffers to support variable non-square sizes
+        if (alloc_vertices[i].texcoord[0]) alloc_vertices[i].texcoord[0] = 256.0f;
+        if (alloc_vertices[i].texcoord[1]) alloc_vertices[i].texcoord[1] = 256.0f;
+    }
 
     /* Create view matrix (our camera is static) */
     matrix_unit(m_view);
@@ -246,15 +262,13 @@ int main(void)
 
     while(1) {
 
-        // poll for input
+        // cycle current texture based on A or B button presses
         SDL_GameControllerUpdate();
-        if (SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_A))
-        {
+        bool aPress = SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_A);
+        bool bPress = SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_B);
+        if (aPress || bPress) {
             if (toggleFormat) {
-
-                // next index, wrapping if necessary
-                format_map_index = (format_map_index + 1) % (sizeof(format_map) / sizeof(format_map[0]));
-                update_texture_memory(format_map[format_map_index].SdlFormat, 256, 256, format_map[format_map_index].XboxSwizzled);
+                format_map_index = (format_map_index + (aPress ? 1 : -1)) % (sizeof(format_map) / sizeof(format_map[0]));
             }
             toggleFormat = false;
         } else toggleFormat = true;
@@ -284,49 +298,24 @@ int main(void)
         p = pb_push1(p, NV097_SET_FRONT_FACE, NV097_SET_FRONT_FACE_V_CCW);
         p = pb_push1(p, NV097_SET_DEPTH_TEST_ENABLE, true);
 
-        // orig RGBA8888
-        /*
-        p = pb_push2(p,NV20_TCL_PRIMITIVE_3D_TX_OFFSET(0),(DWORD)texture.addr & 0x03ffffff,0x0001122a); //set stage 0 texture address & format
-        p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_NPOT_PITCH(0),texture.pitch<<16); //set stage 0 texture pitch (pitch<<16)
-        p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_NPOT_SIZE(0),(texture.width<<16)|texture.height); //set stage 0 texture width & height ((witdh<<16)|height)
+        texture.error = update_texture_memory(format_map[format_map_index].SdlFormat, 256, 256, format_map[format_map_index].XboxSwizzled);
+        DWORD format_mask = MASK(NV097_SET_TEXTURE_FORMAT_CONTEXT_DMA, 1) |
+            MASK(NV097_SET_TEXTURE_FORMAT_CUBEMAP_ENABLE, 0) |
+            MASK(NV097_SET_TEXTURE_FORMAT_BORDER_SOURCE, NV097_SET_TEXTURE_FORMAT_BORDER_SOURCE_COLOR) |
+            MASK(NV097_SET_TEXTURE_FORMAT_DIMENSIONALITY, 2) |
+            MASK(NV097_SET_TEXTURE_FORMAT_COLOR, format_map[format_map_index].XboxFormat) |
+            MASK(NV097_SET_TEXTURE_FORMAT_MIPMAP_LEVELS, 1) |
+            MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_U, format_map[format_map_index].XboxSwizzled ? bsf(texture.width) : 0) |
+            MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_V, format_map[format_map_index].XboxSwizzled ? bsf(texture.height) : 0) |
+            MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_P, 0);
+        p = pb_push2(p,NV20_TCL_PRIMITIVE_3D_TX_OFFSET(0),(DWORD)texture.addr & 0x03ffffff,format_mask); //set stage 0 texture address & format
+        if (!format_map[format_map_index].XboxSwizzled) {
+            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_NPOT_PITCH(0),texture.pitch<<16); //set stage 0 texture pitch (pitch<<16)
+            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_NPOT_SIZE(0),(texture.width<<16)|texture.height); //set stage 0 texture width & height ((witdh<<16)|height)
+        }
         p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_WRAP(0),0x00030303);//set stage 0 texture modes (0x0W0V0U wrapping: 1=wrap 2=mirror 3=clamp 4=border 5=clamp to edge)
         p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_ENABLE(0),0x4003ffc0); //set stage 0 texture enable flags
         p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_FILTER(0),0x04074000); //set stage 0 texture filters (AA!)
-        */
-
-        p = pb_push1(p, NV097_SET_TEXTURE_OFFSET + 0*64, (uint32_t)texture.addr & 0x03ffffff);
-        if (format_map[format_map_index].XboxSwizzled) {
-            p = pb_push1(p, NV097_SET_TEXTURE_FORMAT + 0*64,
-                MASK(NV097_SET_TEXTURE_FORMAT_CONTEXT_DMA, 2) |
-                MASK(NV097_SET_TEXTURE_FORMAT_CUBEMAP_ENABLE, 0) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BORDER_SOURCE, NV097_SET_TEXTURE_FORMAT_BORDER_SOURCE_COLOR) |
-                MASK(NV097_SET_TEXTURE_FORMAT_DIMENSIONALITY, 2) |
-                MASK(NV097_SET_TEXTURE_FORMAT_COLOR, format_map[format_map_index].XboxFormat) |
-                MASK(NV097_SET_TEXTURE_FORMAT_MIPMAP_LEVELS, 1) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_U, bsf(texture.width)) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_V, bsf(texture.height)) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_P, 0));
-
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_WRAP(0),0x00030303); //set stage 0 texture modes (0x0W0V0U wrapping: 1=wrap 2=mirror 3=clamp 4=border 5=clamp to edge)
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_ENABLE(0),0x4003ffc0); //set stage 0 texture enable flags
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_FILTER(0),0x04074000); //set stage 0 texture filters (AA!)
-        } else {
-            p = pb_push1(p, NV097_SET_TEXTURE_FORMAT + 0*64,
-                MASK(NV097_SET_TEXTURE_FORMAT_CONTEXT_DMA, 2) |
-                MASK(NV097_SET_TEXTURE_FORMAT_CUBEMAP_ENABLE, 0) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BORDER_SOURCE, NV097_SET_TEXTURE_FORMAT_BORDER_SOURCE_COLOR) |
-                MASK(NV097_SET_TEXTURE_FORMAT_DIMENSIONALITY, 2) |
-                MASK(NV097_SET_TEXTURE_FORMAT_COLOR, format_map[format_map_index].XboxFormat) |
-                MASK(NV097_SET_TEXTURE_FORMAT_MIPMAP_LEVELS, 1) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_U, 0) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_V, 0) |
-                MASK(NV097_SET_TEXTURE_FORMAT_BASE_SIZE_P, 0));
-
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_NPOT_PITCH(0),texture.pitch<<16); //set stage 0 texture pitch (pitch<<16)
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_NPOT_SIZE(0),(texture.width<<16)|texture.height); //set stage 0 texture width & height ((witdh<<16)|height)
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_ENABLE(0),0x4003ffc0); //set stage 0 texture enable flags
-            p = pb_push1(p,NV20_TCL_PRIMITIVE_3D_TX_FILTER(0),0x04074000); //set stage 0 texture filters (AA!)
-        }
 
         pb_end(p);
 
@@ -390,23 +379,32 @@ int main(void)
          * Setup vertex attributes
          */
 
+        Vertex *vptr = format_map[format_map_index].XboxSwizzled ? alloc_vertices_swizzled : alloc_vertices;
+
         /* Set vertex position attribute */
         set_attrib_pointer(NV2A_VERTEX_ATTR_POSITION, NV097_SET_VERTEX_DATA_ARRAY_FORMAT_TYPE_F,
-                           3, sizeof(Vertex), &alloc_vertices[0].pos);
+                           3, sizeof(Vertex), &vptr[0].pos);
 
         /* Set texture coordinate attribute */
         set_attrib_pointer(NV2A_VERTEX_ATTR_TEXTURE0, NV097_SET_VERTEX_DATA_ARRAY_FORMAT_TYPE_F,
-                           2, sizeof(Vertex), &alloc_vertices[0].texcoord);
+                           2, sizeof(Vertex), &vptr[0].texcoord);
 
         /* Set vertex normal attribute */
         set_attrib_pointer(NV2A_VERTEX_ATTR_NORMAL, NV097_SET_VERTEX_DATA_ARRAY_FORMAT_TYPE_F,
-                           3, sizeof(Vertex), &alloc_vertices[0].normal);
+                           3, sizeof(Vertex), &vptr[0].normal);
         
         /* Begin drawing triangles */
         draw_arrays(NV097_SET_BEGIN_END_OP_TRIANGLES, 0, num_vertices);
 
         /* Draw some text on the screen */
-        pb_print(format_map[format_map_index].Name);
+        pb_print("N: %s\n", format_map[format_map_index].Name);
+        pb_print("F: 0x%x\n", format_map[format_map_index].XboxFormat);
+        pb_print("SZ: %d\n", format_map[format_map_index].XboxSwizzled);
+        pb_print("C: %d\n", format_map[format_map_index].RequireConversion);
+        pb_print("W: %d\n", texture.width);
+        pb_print("H: %d\n", texture.height);
+        pb_print("P: %d\n", texture.pitch);
+        pb_print("ERR: %d\n", texture.error);
         pb_draw_text_screen();
 
         while(pb_busy()) {
@@ -423,6 +421,7 @@ int main(void)
     SDL_GameControllerClose(gameController);
     SDL_QuitSubSystem(SDL_INIT_GAMECONTROLLER);
     MmFreeContiguousMemory(alloc_vertices);
+    MmFreeContiguousMemory(texture.addr);
     pb_show_debug_screen();
     pb_kill();
     return 0;
